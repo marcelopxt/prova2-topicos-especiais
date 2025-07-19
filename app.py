4fb5662e3ce0eecea95a05f2d57f83a62f3590b2
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2025-07-18 21:23:20 -0300

    teste

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def previsao(interpreter, image):
     interpreter.invoke()
     
     # Obtém a saída do modelo — geralmente, as probabilidades para cada classe
-    output_data = Interpreter.get_tensor(output_details[0]['index'])
+    output_data = interpreter.get_tensor(output_details[0]['index'])
 
     # Lista com os nomes das classes, correspondentes à ordem da saída do modelo
     classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_']
